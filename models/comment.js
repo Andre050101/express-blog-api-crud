99b7f658d7f153705b5c6d210a8d9e70001c3efb
Andre050101 <agiancristiano90@gmail.com>
2024-12-04 17:05:12 +0100

    comment.js in models with comments' array added

---
+++ models/comment.js
@@ -0,0 +1,73 @@
+//Array con commenti associati a specifici post
+const comments = [
+    {
+        id: 0,
+        postId: 0,
+        contenuto: "Bellissimo post!",
+        autore: "Mario",
+        data: new Date()
+    },
+    {
+        id: 1,
+        postId: 0,
+        contenuto: "Mi piace molto il modo in cui spieghi le cose.",
+        autore: "Laura",
+        data: new Date()
+    },
+    {
+        id: 2,
+        postId: 1,
+        contenuto: "Non sono d'accordo con alcune cose che hai scritto.",
+        autore: "Giovanni",
+        data: new Date()
+    },
+    {
+        id: 3,
+        postId: 1,
+        contenuto: "Che bello! Questo post mi ha dato molte idee.",
+        autore: "Francesca",
+        data: new Date()
+    },
+    {
+        id: 4,
+        postId: 2,
+        contenuto: "Interessante punto di vista, mi ha fatto riflettere.",
+        autore: "Luca",
+        data: new Date()
+    },
+    {
+        id: 5,
+        postId: 2,
+        contenuto: "Aggiungerei qualche esempio pratico per chiarire meglio.",
+        autore: "Alessandra",
+        data: new Date()
+    },
+    {
+        id: 6,
+        postId: 3,
+        contenuto: "E' un argomento che mi appassiona molto, grazie per il post.",
+        autore: "Matteo",
+        data: new Date()
+    },
+    {
+        id: 7,
+        postId: 3,
+        contenuto: "Molto utile, continuerò a seguirti!",
+        autore: "Elisa",
+        data: new Date()
+    },
+    {
+        id: 8,
+        postId: 4,
+        contenuto: "Penso che il tema sia trattato in modo esaustivo.",
+        autore: "Roberto",
+        data: new Date()
+    },
+    {
+        id: 9,
+        postId: 4,
+        contenuto: "Mi piacerebbe leggere più approfondimenti su questo argomento.",
+        autore: "Sofia",
+        data: new Date()
+    }
+];
