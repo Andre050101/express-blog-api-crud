8a9fcb6c00ec7afe4f4ec0f1e0ba8e7341004433
Andre050101 <agiancristiano90@gmail.com>
2025-01-08 20:51:44 +0100

    adjusted

--- models/post.js
+++ models/post.js
@@ -2,6 +2,8 @@
 const posts = [
     {
         id: 0,
+        author: "Giuseppe",
+        status: "Published",
         title: "Primo post",
         content: "Questo è il contenuto del primo post",
         image: "/assets/ciambellone.jpeg",
@@ -10,6 +12,8 @@ const posts = [
     {
         id: 1,
         title: "Secondo post",
+        author: "Alessio",
+        status: "Published",
         content: "Questo è il contenuto del secondo post",
         image: "/assets/cracker_barbabietola.jpeg",
         tags: ["dolci", "zucchero"]
@@ -17,6 +21,8 @@ const posts = [
     {
         id: 2,
         title: "Terzo post",
+        author: "Andrea",
+        status: "Draft",
         content: "Questo è il contenuto del terzo post",
         image: "/assets/pane_fritto_dolce.jpeg",
         tags: ["dolci", "zucchero"]
@@ -24,6 +30,8 @@ const posts = [
     {
         id: 3,
         title: "Quarto post",
+        author: "Davide",
+        status: "Draft",
         content: "Questo è il contenuto del quarto post",
         image: "/assets/pasta_barbabietola.jpeg",
         tags: ["dolci", "zucchero"]
@@ -31,6 +39,8 @@ const posts = [
     {
         id: 4,
         title: "Quinto post",
+        author: "Giuseppe",
+        status: "Published",
         content: "Questo è il contenuto del quinto post",
         image: "/assets/torta_paesana.jpeg",
         tags: ["dolci", "zucchero"]
